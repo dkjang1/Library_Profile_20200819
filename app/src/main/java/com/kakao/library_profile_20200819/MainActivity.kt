cc7316e5458eac8f59c878a434d4792699a2d876
Jinkyeong96 <69335959+Jinkyeong96@users.noreply.github.com>
2020-08-19 20:01:16 +0900

    프로필사진을 누르면 크게보기화면으로 이동하기

--- app/src/main/java/com/kakao/library_profile_20200819/MainActivity.kt
+++ app/src/main/java/com/kakao/library_profile_20200819/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.kakao.library_profile_20200819
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        profilePhotoImg.setOnClickListener {
+//            사진을 크게보이는 별도의 액티비티로 이동
+            val myIntent = Intent(mContext, ViewProfilePhotoActivity::class.java)
+            startActivity(myIntent)
+        }
 
     }
 
---
+++ app/src/main/java/com/kakao/library_profile_20200819/ViewProfilePhotoActivity.kt
@@ -0,0 +1,20 @@
+package com.kakao.library_profile_20200819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfilePhotoActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_photo)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
