428b1c040b9c08201d10ef2c76da3e310bdc000b
Jinkyeong96 <69335959+Jinkyeong96@users.noreply.github.com>
2020-08-19 22:11:21 +0900

    TedPermission으로 권한을 확인해서 OK인 경우에만 실제 전화 연결

--- app/src/main/java/com/kakao/library_profile_20200819/MainActivity.kt
+++ app/src/main/java/com/kakao/library_profile_20200819/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.kakao.library_profile_20200819
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_view_profile_photo.*
+
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,38 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+
+//            전화기능을 사용가능한상태인지 확인 + 권한요청
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+//                    권한이 승인된 상태일경우 실행할 코드
+//                    Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+//            전화연결처리(ACTION_CALL)
+                    val myUri = Uri.parse("tel:010-5555-6666")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: List<String>) {
+//                    권한이 최종 거부될경우 실행할 코드
+                    Toast.makeText(
+                        mContext,
+                        "전화 권한이 거부되어 연결이 불가능합니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+//            완성된 행동방침을 가지고 권한실제요청
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("권한을 거부하면 통화가 불가능합니다.\n설정 >  권한을 허용해 주세요")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
 
         profilePhotoImg.setOnClickListener {
 //            사진을 크게보이는 별도의 액티비티로 이동
