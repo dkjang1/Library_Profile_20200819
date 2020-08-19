7d54ac168647388996ac48a8be6acd3a33699903
Jinkyeong96 <69335959+Jinkyeong96@users.noreply.github.com>
2020-08-19 21:10:34 +0900

    Glide 라이브러리 웹이미지 불러오기

--- app/src/main/java/com/kakao/library_profile_20200819/MainActivity.kt
+++ app/src/main/java/com/kakao/library_profile_20200819/MainActivity.kt
@@ -3,7 +3,9 @@ package com.kakao.library_profile_20200819
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_view_profile_photo.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +26,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        Glide.with(mContext)
+            .load("https://img.extmovie.com/files/attach/images/148/767/645/051/cf71cbb965d6ee8273da5602f255eb74.jpg")
+            .into(profilePhotoImg);
 
     }
 
